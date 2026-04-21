1882ed26956da3cdc17e29f00e39c8c46443a662
brunocandidolira <bruno.georgecandido@gmail.com>
2026-04-21 14:43:59 -0300

    refactor: implement status code handling in error.js

--- src/controller/error.js
+++ src/controller/error.js
@@ -1,11 +1,41 @@
-export const badRequestError = (message) => {
-    const error = new Error(message);
-    error.status = 400;
-    return error;
-};
+export const badRequestError = (body) => ({
+    status: 400,
+    body,
+});
 
-export const conflictError = (message) => {
-    const error = new Error(message);
-    error.status = 409;
-    return error;
-};
+
+export const conflictError = (body) => ({
+    status: 409,
+    body,
+});
+
+export const  ok =(body)=>({
+    status:200,
+    body
+    });
+    export const internalServerError = (body) => ({
+        status: 500,
+        body: {
+            error: body instanceof Error ? body.message : body,
+        },
+    });
+    export const created = (body) => ({
+        status: 201,
+        body,
+    });
+    export const notFoundError = (body) => ({
+        status: 404,
+        body,
+    });
+    export const unauthorizedError = (body) => ({
+        status: 401,
+        body,
+    });
+    export const forbiddenError = (body) => ({
+        status: 403,
+        body,
+    });
+    export const unprocessableEntityError = (body) => ({
+        status: 422,
+        body,
+    });    
